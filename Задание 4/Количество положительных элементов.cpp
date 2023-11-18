189e0cf6a58676a5bd529d49c91f3f2d8bb2f15d
st117213 <145061624+Alinafrolova05@users.noreply.github.com>
2023-11-18 21:59:23 +0300

    Add files via upload

---
+++ Задание 4/Количество положительных элементов.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main(int argc, char** )
+{
+	int size = 0;
+	std::cout << "Укажите размер массива: ";
+	std::cin >> size;
+	int* a = (int*)malloc(1000 * sizeof(int));
+	std::cout << std::endl;
+	std::cout << "Введите числа, которые будут находиться в массиве: ";
+	std::cout << std::endl;
+	for (int i = 0; i < size; ++i)
+	{
+		int n = 0;
+		std::cin >> n;
+		*(a + i) = n;
+	}
+	int p = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if ((*(a + i)) > 0)
+		{
+			p++;
+		}
+	}
+	std::cout << "Количество положительных элементов в массиве: " << p;
+	free(a);
+	return EXIT_SUCCESS;
+}
