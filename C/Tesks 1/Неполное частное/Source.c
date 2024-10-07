ac823fc5441bf6e4d9ef9ce4765b6f8f559cea9e
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-10-07 11:10:08 +0300

    New files

--- C/Tesks 1/Неполное частное/Source.c
+++ C/Tesks 1/Неполное частное/Source.c
@@ -2,33 +2,52 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
-void Dividing(int a, int b) {
-    if (b == 0) {
-        printf("Error");
-    }
-    if (a == 0) {
-        printf("0");
-    } else {
-        int i = -abs(a);
-        while (1) {
-            if (a - b * i >= 0 && a - b * i < abs(b)) {
-                printf("Incomplete quotient: %d", i);
-                break;
-            }
-            ++i;
+bool testResult(int result) {
+    return 1;
+}
+bool test(int b) {
+    return b != 0;
+}
+
+void divide(int a, int b, int* quotient) {
+    int i = -abs(a);
+    while (true) {
+        if (a - b * i >= 0 && a - b * i < abs(b)) {
+            *quotient = i;
+            break;
         }
+        ++i;
     }
 }
 
 int main(void)
 {
-    const int a = 0;
-    const int b = 0;
+    int a = 1;
+    int b = 1;
     printf("Enter the dividend: ");
     int result = scanf("%d", &a);
+    if (!testResult(result)) {
+        printf("Input error!");
+    }
+
     printf("Enter the divisor: ");
     result = scanf("%d", &b);
-    Dividing(a, b);
+    if (!testResult(result)) {
+        printf("Input error!");
+    }
+
+    if (!test(b)) {
+        printf("Error! Divisor cannot be zero!");
+        return 0;
+    }
+    if (!test(a)) {
+        printf("0");
+    }
+
+    int quotient = 1;
+    divide(a, b, &quotient);
+    printf("Incomplete quotient: %d", quotient);
     return 0;
 }
