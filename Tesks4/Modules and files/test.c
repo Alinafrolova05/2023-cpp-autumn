2eaadc32d4c5eecbb2c35ea0fa3a8a0b2ff50874
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-10-28 22:17:23 +0300

    Delete

--- Tesks4/Modules and files/test.c
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-#include <stdbool.h>
-bool testScanf(int result) {
-    return result == 1;
-}
-bool test(int size, int checkSizeInteger) {
-    return size == checkSizeInteger && size > 0;
-}
