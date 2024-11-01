e436f7a75fa24e77eb336f76fd6bf940b25c7748
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-11-01 16:51:16 +0300

    New files

---
+++ test/Task3/Task3.c
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "module.h"
+
+int main()
+{
+    FILE* file1 = fopen("input.txt", "r");
+    FILE* file2 = fopen("output.txt", "w");
+    if (!testFile(file1)) {
+        printf("Error!");
+        return -1;
+    }
+    if (!testFile(file2)) {
+        printf("Error!");
+        fclose(file1);
+        return -1;
+    }
+    char str[100] = { 0 };
+    fillArray(file1, str);
+    printInFile(file2, str);
+    fclose(file1);
+    fclose(file2);
+    return 0;
+}
---
+++ test/Task3/module.h
@@ -0,0 +1,7 @@
+#ifndef H
+#include <stdbool.h>
+#include <stdio.h>
+bool testFile(FILE* file);
+void fillArray(FILE* file1, char str[]);
+void printInFile(FILE* file2, char str[]);
+#endif
