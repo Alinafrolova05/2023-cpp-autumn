af8ce1428f9cdf8ca7c2d078365bf01049f5ccc6
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-11-04 01:48:30 +0300

    New files

---
+++ Tasks5/Postfix_calculator/Postfix_calculator.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include "functions.h"
+
+int main()
+{
+    int answer = 0;
+    Element* element1 = calloc(1, sizeof(Element));
+    if (!test(element1, &answer)) {
+        printf("\nError!!!");
+        return -1;
+    }
+    char str[100] = { 0 };
+    printf("Enter numbers and operations: ");
+    gets(str, 100, stdin);
+    numbers(element1, str, &answer);
+    printf("\nAnswer = %d", answer);
+    free(element1);
+    return 0;
+}
---
+++ Tasks5/Postfix_calculator/functions.c
@@ -0,0 +1,54 @@
+#include "functions.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool testZero(int number) {
+    return number != 0;
+}
+void push(Element** head, int value) {
+    Element* element = calloc(1, sizeof(Element));
+    element->value = value;
+    element->next = *head;
+    *head = element;
+}
+void pop(Element** head2) {
+    Element* tmp = *head2;
+    *head2 = (*head2)->next;
+    free(tmp);
+}
+void operation(Element* element1, char str[], int i, int* answer) {
+    if (str[i] == '+') {
+        element1->next->value += element1->value;
+    }
+    if (str[i] == '-') {
+        element1->next->value -= element1->value;
+    }
+    if (str[i] == '/') {
+        if (!testZero(element1->value)) {
+            printf("\nError!!!");
+            return;
+        }
+        element1->next->value /= element1->value;
+    }
+    if (str[i] == '*') {
+        element1->next->value *= element1->value;
+    }
+    *answer = element1->next->value;
+}
+void numbers(Element* element1, char str[], int* answer) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '/' || str[i] == '*') {
+            operation(element1, str, i, answer);
+            pop(&element1);
+        }
+        else {
+            int number = (int)(str[i]) - 48;
+            push(&element1, number);
+        }
+    }
+}
+bool test(Element* element1, int answer) {
+    char str[20] = "96-12+*";
+    numbers(element1, str, &answer);
+    return answer == 9;
+}
