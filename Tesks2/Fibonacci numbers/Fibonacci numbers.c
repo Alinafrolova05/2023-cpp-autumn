78b9628d17b26e340a71866e8cc83c5e6bdc9db4
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-12-20 01:41:31 +0300

    New files

--- Tesks2/Fibonacci numbers/Fibonacci numbers.c
+++ Tesks2/Fibonacci numbers/Fibonacci numbers.c
@@ -4,10 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-bool testScanf(int result) {
-    return result == 1;
-}
-bool testPositiveNumber(int number) {
+bool isPositive(int number) {
     return number >= 0;
 }
 
@@ -27,7 +24,7 @@ int fibonacciRecursion(int number, int *result) {
     return 0;
 }
 
-bool testRecurtion() {
+bool testRecurtion(void) {
     int result = 0;
     int check1 = fibonacciRecursion(5, &result);
     int check2 = fibonacciRecursion(10, &result);
@@ -38,7 +35,7 @@ bool testRecurtion() {
 int fibonacciIteration(int number, int* result) {
     int fibonacci1 = 0;
     int fibonacci2 = 1;
-    int fibonaccimain = 0;
+    int fibonaccimain = number;
     for (int i = 0; i < number; ++i) {
         fibonaccimain = fibonacci1 + fibonacci2;
         fibonacci2 = fibonacci1;
@@ -53,26 +50,29 @@ int fibonacciIteration(int number, int* result) {
     }
 }
 
-bool testIteration() {
+bool testIteration(void) {
     int result = 0;
     int check1 = fibonacciIteration(5, &result);
     int check2 = fibonacciIteration(-5, &result);
-    return check1 == 0 && check2 == 0;
+    return check1 == 0 && check2 == 1;
 }
 
-void countTime(int i, double* pointer1, double* pointer2) {
+void countTimeOfRecursion(int i, double* pointer) {
     int result = 0;
     clock_t start1 = clock();
     fibonacciRecursion(i, &result);
     clock_t end1 = clock();
     double recursion = 1000.0 * (end1 - start1) / CLOCKS_PER_SEC;
-    *pointer1 = recursion;
+    *pointer = recursion;
+}
 
+void countTimeOfIteration(int i, double* pointer) {
+    int result = 0;
     clock_t start2 = clock();
     fibonacciIteration(i, &result);
     clock_t end2 = clock();
     double iteration = 1000.0 * (end2 - start2) / CLOCKS_PER_SEC;
-    *pointer2 = iteration;
+    *pointer = iteration;
 }
 
 int main(void)
@@ -81,15 +81,15 @@ int main(void)
     printf("Enter the fibonacci number you want to know: ");
     int number = 1;
     int resultScanf = scanf("%d", &number);
-    if (!testScanf(resultScanf)) {
+    if (resultScanf != 1) {
         printf("Input error!");
         return 0;
     }
-    if (!testPositiveNumber(number)) {
+    if (!isPositive(number)) {
         printf("The number must be positive!");
         return 0;
     }
-    if (!testRecurtion() || testIteration()) {
+    if (!testRecurtion() || !testIteration()) {
         printf("The sequence does not count!");
         return 0;
     }
@@ -104,7 +104,8 @@ int main(void)
         double recursion = 0;
         double iteration = 0;
 
-        countTime(i, &recursion, &iteration);
+        countTimeOfRecursion(i, &recursion);
+        countTimeOfIteration(i, &iteration);
 
         if (recursion > iteration + 100) {
             printf("\nFrom the %dth Fibonacci number, the recursive option is slower than the iterative one.\n", i);
