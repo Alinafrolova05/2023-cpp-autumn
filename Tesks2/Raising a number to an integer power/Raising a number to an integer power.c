f63f762022cbd3af49a8c43c89c1833c8384b827
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-12-20 20:32:31 +0300

    New files

--- Tesks2/Raising a number to an integer power/Raising a number to an integer power.c
+++ Tesks2/Raising a number to an integer power/Raising a number to an integer power.c
@@ -3,97 +3,47 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool testInteger(float power, int additionalbool) { 
+int calculatePowerLog(float number, int power) {
     if (power < 0) {
+        number = 1 / number;
         power = -power;
     }
-    return power != additionalbool;
-}
 
-void calculationPowerLog(float number, float *pointernumber, float staticNumber, int additionalbool, int* pointeradditionalbool,  float power) {
-    if (power < 0) {
-        power = -power;
-    }
-    for (size_t i = 1; i < power / 2; ++i) {
-        number *= staticNumber;
-        additionalbool++;
-    }
-    if ((additionalbool != power / 2) && staticNumber != 0) {
-        number *= number;
-        number /= staticNumber;
-        additionalbool += additionalbool - 1;
-        if (additionalbool < power) {
-            number *= staticNumber;
+    float result = 1;
+
+    while (power > 0) {
+        if (power % 2 == 1) {
+            result *= number;
         }
-    }
-    if (additionalbool == power / 2) {
         number *= number;
-        additionalbool += additionalbool;
+        power /= 2;
     }
-    *pointernumber = number;
-    *pointeradditionalbool = additionalbool;
+    return result;
 }
 
-void calculationPowerLin(float number, float *pointernumber, float staticNumber, float power, int additionalbool, int *pointeradditionalbool) {
+int calculatePowerLin(float number, int power) {
     if (power < 0) {
+        number = 1 / number;
         power = -power;
-	}
-    for (size_t i = 1; i < power; ++i) {
-        number *= staticNumber;
-        additionalbool++;
     }
-    *pointernumber = number;
-    *pointeradditionalbool = additionalbool;
+
+    float number1 = number;
+
+    for (int i = 1; i < power; ++i) {
+        number *= number1;
+    }
+
+    return number;
 }
 
-void print(float number, float power) {	
-    if (power > 0) {
-        printf(" %f ", number);
-    }
-    else if (power < 0) {
-        printf(" %f ", 1 / number);
-    }
-    else {
-        printf(" 1 ");
-    }
+bool test(void) {
+    return calculatePowerLog(2, 5) == 32 && calculatePowerLin(2, 5) == 32;
 }
 
-int main()
-{
-    printf("Enter number: ");
-    float number = 0;
-    int result = scanf("%f", &number);
-    if (result != 1) {
-        printf("Input error!");
-        return 0;
+int main(void) {
+    if (!test()) {
+        printf("Error!");
+        return -1;
     }
-
-    printf("Enter an integer that will be the power: ");
-    float power = 0;
-    result = scanf("%f", &power);
-    if (!testScanf(result)) {
-        printf("Input error!");
-        return 0;
-    }
-   
-    int additionalbool = 1;
-    float staticNumber = number;
-
-    calculationPowerLog(number, &number, staticNumber, additionalbool, &additionalbool,  power);
-    //calculationPowerLin(number, &number, staticNumber, power, additionalbool, &additionalbool);
-
-    if (testInteger(power, additionalbool) && power != 0) {
-        printf("Input error, power is not an integer!!!\nNumber to the integer part of the power: ");
-        if (staticNumber == 0 ) {
-            print(number, power);
-            return 0;
-        } else {
-            print(number / staticNumber, power);
-            return 0;
-        }
-    }
-     
-    printf("\nNumber %f to the power %f: ", staticNumber, power);
-    print(number, power);
     return 0;
 }
