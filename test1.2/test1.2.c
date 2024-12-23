8c87c1ab1fe2e181fa7e1b8d48d051db75815e9e
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2024-12-23 13:34:01 +0300

    New files

---
+++ test1.2/test1.2.c
@@ -0,0 +1,82 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int count(int array[], int size, int array2[], int* size2) {
+    int max = 0;
+    *size2 = 0;
+
+    for (int i = 0; i < size; ++i) {
+        int count = 0;
+        int number = array[i];
+        int original_number = number;
+
+        while (number != 0) {
+            count += number % 10;
+            number /= 10;
+        }
+
+        if (count > max) {
+            max = count;
+            *size2 = 1;
+            array2[0] = original_number;
+        }
+        else if (count == max) {
+            array2[*size2] = original_number;
+            (*size2)++;
+        }
+    }
+
+    return max;
+}
+
+bool test(void) {
+    int array[] = { 1, 5, 78, 3059, 1000 };
+    int expected_max = 17;
+    int size2 = 0;
+    int array2[10] = { 0 };
+    int result = count(array, sizeof(array) / sizeof(array[0]), array2, &size2);
+    return result == expected_max;
+}
+
+void program(void) {
+    int array[256] = { 0 };
+    int size = 0;
+
+    printf("Enter size: ");
+    if (scanf("%d", &size) != 1 || size <= 0 || size > 256) {
+        printf("Error!\n");
+        return;
+    }
+
+    printf("Enter elements of array: ");
+    for (int i = 0; i < size; ++i) {
+        if (scanf("%d", &array[i]) != 1) {
+            printf("Error!\n");
+            return;
+        }
+    }
+
+    int array2[256] = { 0 };
+    int size2 = 0;
+
+    int max_sum = count(array, size, array2, &size2);
+
+    printf("\nMaximum sum of digits: %d\n", max_sum);
+    printf("Numbers with this sum: ");
+    for (int i = 0; i < size2; ++i) {
+        printf("%d ", array2[i]);
+    }
+}
+
+int main(void) {
+
+    if (!test()) {
+        printf("Error!\n");
+    }
+
+    program();
+
+    return 0;
+}
---
+++ test2.22/test2.22.c
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int* left, int* right) {
+    if (left == right) {
+        return;
+    }
+    *left ^= *right;
+    *right ^= *left;
+    *left ^= *right;
+}
+
+void selectionSort(int size, int array[]) {
+    for (int i = 0; i < size - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(&array[i], &array[minIndex]);
+    }
+}
+
+bool test(void) {
+    int array[10] = { 2, 7, 4, 56, 3 };
+    int checkArray[10] = { 2, 3, 4, 7, 56 };
+    int size = 5;
+    selectionSort(size, array);
+    for (int i = 0; i < size; ++i) {
+        if (array[i] != checkArray[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void program(void) {
+    int array[10] = { 0 };
+    printf("Enter 10 elements in the array: ");
+    for (int i = 0; i < 10; ++i) {
+        if (scanf("%d", &array[i]) != 1) {
+            printf("Error!\n");
+            return;
+        }
+    }
+    selectionSort(10, array);
+    printf("Resulting array: ");
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", array[i]);
+    }
+}
+
+int main(void) {
+    
+    if (!test()) {
+        printf("Error!");
+        return -1;
+    }
+
+    program();
+    
+    return 0;
+}
