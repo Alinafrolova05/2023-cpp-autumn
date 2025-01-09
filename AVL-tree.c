86c18963734a3aa18c1703a6b2db48ab727bad76
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2025-01-09 16:52:33 +0300

    refactor: the function for deleting a node has been changed.

---
+++ AVL-tree.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tree.h"
+
+int main(void) {
+    Node* root = NULL;
+
+    solution(&root);
+    freeTree(&root);
+
+    return 0;
+}
+
