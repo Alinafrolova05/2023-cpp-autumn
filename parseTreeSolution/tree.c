9226c86af347c1fee8da410003de4ecc9799d3ad
Alinafrolova05 <145061624+Alinafrolova05@users.noreply.github.com>
2025-03-05 23:34:13 +0300

    refactor: new modules added

---
+++ parseTreeSolution/tree.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct Tree {
+    char value;
+    int result;
+    struct Tree* leftChild;
+    struct Tree* rightChild;
+} Tree;
+
+typedef struct Stack {
+    Tree* element;
+    struct Stack* next;
+} Stack;
+
+void freeTree(Tree** element) {
+    if (*element == NULL) return;
+    freeTree(&((*element)->leftChild));
+    freeTree((&(*element)->rightChild));
+    free(*element);
+}
+
+void push(Stack** head, Tree* element, bool* errorCode) {
+    Stack* newStackNode = (Stack*)calloc(1, sizeof(Stack));
+    if (newStackNode == NULL) {
+        *errorCode = false;
+        return;
+    }
+    newStackNode->element = element;
+    newStackNode->next = *head;
+    *head = newStackNode;
+}
+
+Tree* pop(Stack** head) {
+    if (*head == NULL) return NULL;
+    Stack* tmp = *head;
+    Tree* element = tmp->element;
+    *head = (*head)->next;
+    free(tmp);
+    return element;
+}
+
+Tree* createNode(char value, bool* errorCode) {
+    Tree* newNode = (Tree*)calloc(1, sizeof(Tree));
+    if (newNode == NULL) {
+        *errorCode = false;
+        return NULL;
+    }
+    newNode->value = value;
+    newNode->leftChild = NULL;
+    newNode->rightChild = NULL;
+    return newNode;
+}
+
+Tree* tree(char* str, bool* errorCode) {
+    Stack* stream = NULL;
+    int size = strlen(str);
+    for (int i = size - 1; i >= 0; --i) {
+        char currentChar = str[i];
+        if (isspace(currentChar)) {
+            continue;
+        }
+        if (isdigit(currentChar)) {
+            Tree* newNode = createNode(currentChar, errorCode);
+            if (!*errorCode) {
+                return NULL;
+            }
+            newNode->result = currentChar - '0';
+            push(&stream, newNode, errorCode);
+            if (!*errorCode) {
+                return NULL;
+            }
+        }
+        else if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
+            Tree* operatorNode = createNode(currentChar, errorCode);
+            if (!*errorCode) {
+                return NULL;
+            }
+            operatorNode->leftChild = pop(&stream);
+            operatorNode->rightChild = pop(&stream);
+            push(&stream, operatorNode, errorCode);
+            if (!*errorCode) {
+                return NULL;
+            }
+        }
+    }
+    return pop(&stream);
+}
+
+void printTree(Tree* element) {
+    if (element == NULL) return;
+    if (element->leftChild != NULL || element->rightChild != NULL) {
+        printf("(");
+    }
+    printf("%c", element->value);
+    printTree(element->leftChild);
+    printTree(element->rightChild);
+    if (element->leftChild != NULL || element->rightChild != NULL) {
+        printf(")");
+    }
+}
+
+int count(Tree* element, bool* errorCode) {
+    if (element == NULL) return 0;
+
+    count(element->leftChild, errorCode);
+    count(element->rightChild, errorCode);
+    if (element->value == '+') { 
+        element->result = element->leftChild->result + element->rightChild->result;
+    }
+    else if (element->value == '-') {
+        element->result = element->leftChild->result - element->rightChild->result;
+    }
+    else if (element->value == '*') {
+        element->result = element->leftChild->result * element->rightChild->result;
+    }
+    else if (element->value == '/') {
+        if (element->rightChild->result != 0) {
+            element->result = element->leftChild->result / element->rightChild->result;
+        }
+        else {
+            *errorCode = false;
+            return -1;
+        }
+    }
+    return element->result;
+}
+
+int countResult(char* str, bool* errorCode) {
+    Tree* element = tree(str, errorCode);
+    return count(element, errorCode);
+}
---
+++ parseTreeSolution/tree.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdbool.h>
+
+// Defines the structure.
+typedef struct Tree Tree;
+
+// Prints tree.
+void printTree(Tree* element);
+
+// Builds a tree.
+Tree* tree(char* str, bool* errorCode);
+
+// Calculates the result of an arithmetic expression.
+int countResult(char* str, bool* errorCode);
+
+// Removes a tree.
+void freeTree(Tree** element);
